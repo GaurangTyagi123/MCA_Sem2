52677f97b00e7445f80f4192d2da11211a77052c
GaurangTyagi <gaurangtyagi@gaurang.work>
2026-02-12 00:27:28 +0530

    UPDATE : DAA practical 3

--- DAA/practicals/R_No_16_P3c_GaurangTygai.cpp
+++ DAA/practicals/R_No_16_P3c_GaurangTygai.cpp
@@ -29,8 +29,8 @@ int main()
 {
     // parameters
     std::string schemaStr = "age:int,job:str,marital:str,education:str,default:str,balance:int,housing:str,loan:str,contact:str,day:str,month:str,duration:str,campaign:str,pdays:str,previous:int,poutcome:str,y:str";
-    std::string filePath = "./Data/practical_2_data/bank_full_mini.csv";
-    std::string outputPath = "./Data/practical_2_data/bank_mini.csv";
+    std::string filePath = "./Data/practical_2_data/bank-full.csv";
+    std::string outputPath = "./Data/practical_2_data/bank.csv";
 
     ReportType rep = applySort(schemaStr, filePath, outputPath, "age");
     std::cout << "Number of comparisons: " << rep.comparisons << std::endl;
